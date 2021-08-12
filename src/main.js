1bf8c1e44c1dde43fd63ed019d7b221378f6b8d8
Jonathan Arias <jonalxh@gmail.com>
2021-08-11 21:07:14 -0500

    Working web component

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import wrap from "@vue/web-component-wrapper"
 import App from './App.vue'
 
-Vue.config.productionTip = false
+const wrappedElement = wrap(Vue, App);
+
+window.customElements.define("v-simple-timeago", wrappedElement);
+
+/* Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app') */
